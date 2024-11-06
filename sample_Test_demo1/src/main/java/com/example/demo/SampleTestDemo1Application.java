9db2db5646839f843dfe1ea090a59ef68828f093
한승민 <gkstmddk131@naver.com>
2024-11-06 09:01:53 +0900

    프로젝트 최초 생성

---
+++ sample_Test_demo1/src/main/java/com/example/demo/SampleTestDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTestDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTestDemo1Application.class, args);
+	}
+
+}
